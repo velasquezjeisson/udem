1b8fa6e14062d34cf9483669f371926db07aa334
unknown <velasquezjeisson@gmail.com>
2025-05-08 18:22:34 -0500

    ignore

--- Proyecto 1/README.md
+++
@@ -1,2 +0,0 @@
-Recuerden instalar pandas en vscode con: pip install pandas. 
-Instalar extenciones que te recomienda VSC al iniciar una celda (Python + Jupyter)
--- Proyecto 2/infrastructure/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "app_bucket" {
-  bucket = var.bucket_name
-
-  tags = {
-    Name        = "${var.project_name}-bucket"
-    Environment = "Dev"
-  }
-}
