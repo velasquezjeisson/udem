005ac2b2ce95ac8f297abc3de89c9098935d11af
unknown <velasquezjeisson@gmail.com>
2025-05-08 19:53:40 -0500

    cambios user .sh

--- Proyecto 2/infrastructure/user_data.sh
+++ Proyecto 2/infrastructure/user_data.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
+
+# Guardar salida en log
 exec > /var/log/user_data.log 2>&1
 set -x
 
-yum update -y
-yum install -y git curl
+# Eliminar curl minimal si existe
+yum remove -y curl-minimal || true
 
-# Instalar uv para ec2-user
-sudo -u ec2-user -i <<'EOF'
+# Instalar dependencias necesarias
+yum install -y git curl unzip
+
+# Instalar uv como ec2-user
+sudo -u ec2-user -i -- bash <<'EOF'
+cd /home/ec2-user
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
-cd /home/ec2-user
+# Clonar el repositorio
 git clone https://github.com/velasquezjeisson/udem.git
+
+# Entrar al proyecto
 cd "udem/Proyecto 2/src"
+
+# Sincronizar dependencias
 uv sync
 EOF
