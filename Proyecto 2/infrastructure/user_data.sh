00247e811cf479ef4ae6bb0e0cdd86e01ae7202c
unknown <velasquezjeisson@gmail.com>
2025-05-15 18:51:30 -0500

    RDS SQL

--- Proyecto 2/infrastructure/user_data.sh
+++ Proyecto 2/infrastructure/user_data.sh
@@ -31,6 +31,26 @@ cd "/home/ec2-user/udem/Proyecto 2"
 uv venv
 uv sync
 
+
+# Instalar dependencias para ODBC
+sudo yum install -y gcc-c++ gcc unixODBC-devel python3-devel curl
+
+# Agregar repositorio Microsoft e instalar driver
+curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/msprod.repo
+sudo ACCEPT_EULA=Y yum install -y msodbcsql17
+
+# Activar entorno virtual
+source .venv/bin/activate
+
+# Instalar paquetes Python
+pip install pandas pyodbc requests
+
+# Ejecutar script de carga de Excel a RDS
+python /home/ec2-user/udem/Proyecto\ 2/scripts/cargar_excel_a_rds.py
+
+
+
+
 # Ejecutar entrenamiento
 cd src
 ../.venv/bin/python train.py
@@ -42,4 +62,8 @@ nohup ../.venv/bin/uvicorn main:app --host 0.0.0.0 --port 8000 > api.log 2>&1 &
 # Lanzar Streamlit (en otro puerto)
 cd /home/ec2-user/udem/Proyecto\ 2
 nohup .venv/bin/streamlit run app.py --server.port 8501 --server.headless true > streamlit.log 2>&1 &
+
+
+
+
 EOF
