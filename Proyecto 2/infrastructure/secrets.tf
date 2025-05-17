e384a98a2f17fa41538052ff893a8f6ee71b53b6
unknown <velasquezjeisson@gmail.com>
2025-05-16 19:03:31 -0500

    rds secret v2

--- Proyecto 2/infrastructure/secrets.tf
+++ Proyecto 2/infrastructure/secrets.tf
@@ -1,12 +1,18 @@
 resource "aws_secretsmanager_secret" "sql_credentials" {
-  name        = "proyecto2/sqlserver-v2"  # ← cámbialo si el anterior está en pending deletion
-  description = "Credenciales para la instancia RDS SQL Server"
-
+  name        = "proyecto2/sqlserver-v2"
+  description = "Credenciales para SQL Server RDS"
   lifecycle {
-    prevent_destroy = true  # ← impide que el secreto se destruya con `terraform destroy`
+    prevent_destroy = true
   }
-
   tags = {
     Name = "proyecto2/sqlserver"
   }
+}
+
+resource "aws_secretsmanager_secret_version" "sql_credentials_version" {
+  secret_id     = aws_secretsmanager_secret.sql_credentials.id
+  secret_string = jsonencode({
+    username = "adminuser",
+    password = var.db_password
+  })
 }
